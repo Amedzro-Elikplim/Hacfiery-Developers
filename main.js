dfa58d98599cb4fabd9c0d12aa09a4eeeaef73ea
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-11 15:06:38 +0000

    Create featured speakers section

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const featuredSpeakers = document.querySelector('.speakers');
+
+function featuredSectionTitle() {
+  const title = document.createElement('h2');
+  title.innerHTML = "Featured Speakers"
+  return title;
+}
+
+function horizontalLine() {
+  const hr = document.createElement('hr');
+  hr.className = 'line';
+  return hr;
+}
