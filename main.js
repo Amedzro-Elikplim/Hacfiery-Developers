0f785ecbc370d334170cf44ed0e9977b6fc72213
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-11 21:33:00 +0000

    Add aos library for animation

--- main.js
+++ main.js
@@ -3,38 +3,38 @@ const featuredSpeakers = document.querySelector('.speakers');
 const speakerInfo = [
   {
     name: 'Betsy Nunoo',
+    image: './assets/images/betsy.jfif',
+    profession: 'CEO of Hackfiery, Studies Computer science at Turkey',
+    about: 'Betsy Nunoo, is currently doing her Masters in computer science. She publised papers on the use of quantum computers',
+  },
+  {
+    name: 'Paul Kofi Osei',
+    image: './assets/images/paul.jpg',
+    profession: 'Co-founder of Hackfiery, owns a coding hub that trains youth in cloud services',
+    about: 'Paul Kofi, osei, is currently doing his PhD in cloud computing. He is the author of cloud computing now!. The bestseller of New york times',
+  },
+  {
+    name: 'Dowell Wisdom',
+    image: './assets/images/jayden.jpg',
+    profession: 'Software Engineer, Facebook',
+    about: 'Dowell Wisdom, owns and maintains an open source library known as jscript. He is a publisher and author of several books on programming',
+  },
+  {
+    name: 'Sharon Dassah',
+    image: './assets/images/sharon.jfif',
+    profession: 'Co-founder of Hackfiery and Dropbox',
+    about: 'Sharon Dassah, is currently doing his Masters in cloud computing in New Mexico. He is the author of cloud computing now!. The bestseller of New york times',
+  },
+  {
+    name: 'Julliet Ibrahim',
     image: './assets/images/ceo.jpg',
-    profession: 'CEO of Hackfiery, Studies Computer science at Turkey Institute of Computer science',
-    about: 'Betsy Nunoo, is currently doing her Masters in computer science. She publised papers on the use of quantum computers to solve immigration problems',
+    profession: 'Software Engineer, Google developer',
+    about: 'Peter Sikpa, owns and maintains an open source library known as jscript. He is a publisher and author of several books on programming',
   },
   {
-    name: 'Paul Kofi Osei',
-    image: './assets/images/paul.jpg',
-    profession: 'Co-founder of Hackfiery, owns a coding hub that trains youth in cloud services',
-    about: 'Paul Kofi, osei, is currently doing his PhD in cloud computing. He is the author of cloud computing now!. The bestseller of New york times',
-  },
-  {
-    name: 'Jayden Brown',
-    image: './assets/images/jayden.jpg',
-    profession: 'CTO of Hackfiery, open-source developer',
-    about: 'Jayden Brown, owns and maintains an open source library known as jscript. He is a publisher and author of several books on programming',
-  },
-  {
-    name: 'Paul Kofi Osei',
-    image: './assets/images/paul.jpg',
-    profession: 'Co-founder of Hackfiery, owns a coding hub that trains youth in cloud services',
-    about: 'Paul Kofi, osei, is currently doing his PhD in cloud computing. He is the author of cloud computing now!. The bestseller of New york times',
-  },
-  {
-    name: 'Jayden Brown',
-    image: './assets/images/jayden.jpg',
-    profession: 'CTO of Hackfiery, open-source developer',
-    about: 'Jayden Brown, owns and maintains an open source library known as jscript. He is a publisher and author of several books on programming',
-  },
-  {
-    name: 'Jayden Brown',
-    image: './assets/images/jayden.jpg',
-    profession: 'CTO of Hackfiery, open-source developer',
+    name: 'Thomas Bright',
+    image: './assets/images/thomas.jpg',
+    profession: 'Full Stack Developer, Microverse',
     about: 'Jayden Brown, owns and maintains an open source library known as jscript. He is a publisher and author of several books on programming',
   },
 ];
@@ -98,6 +98,7 @@ function moreButton() {
   const button = document.createElement('button');
   button.innerHTML = 'More <i class="bi bi-chevron-down button-icon"></i>';
   button.className = 'more-button';
+  button.setAttribute('data-aos', 'fade-up');
 
   return button;
 }
@@ -108,6 +109,7 @@ function createSpeaker(data) {
 
   speakerCardTextContent.className = 'speaker-card-text-content';
   speakersCard.className = 'speakers-card';
+  speakersCard.setAttribute('data-aos', 'zoom-in');
   speakerName(data);
   speakerProfession(data);
   speakerLine();
